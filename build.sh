eb41e1e1670d14d3fdf6f80803ab1384467d8c21
François Triquet <francois.triquet01@gmail.com>
2019-01-25 00:38:31 +0100

    add build script

---
+++ build.sh
@@ -0,0 +1,6 @@
+cargo build --release --target wasm32-unknown-unknown
+wasm-bindgen \
+  target/wasm32-unknown-unknown/release/game_of_life.wasm \
+  --out-dir build \
+  --no-modules \
+  --no-modules-global gameOfLife
