8f570ac4ab2a1debca13c336b9c1d7bd513c7f6f
François Triquet <francois.triquet01@gmail.com>
2019-02-09 00:33:03 +0100

    a

---
+++ build/game_of_life_bg.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_world_free(a: number): void;
+export function world_load_string(a: number, b: number, c: number, d: number): void;
+export function world_set_mode(a: number, b: number): void;
+export function world_generations(a: number): number;
+export function world_clear(a: number): void;
+export function world_toggle(a: number, b: number, c: number): void;
+export function world_cells(a: number): number;
+export function world_changed_cells(a: number): number;
+export function world_changed_cells_len(a: number): number;
+export function world_reset_changed_cells(a: number): void;
+export function world_tick(a: number): void;
+export function world_new(a: number, b: number): number;
+export function __wbindgen_global_argument_ptr(): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
