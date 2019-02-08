beb8426ebebd1694a8a005b28f9beed87355af18
François Triquet <francois.triquet01@gmail.com>
2019-02-08 23:48:32 +0100

    ci

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-cargo +nightly build --release --target wasm32-unknown-unknown
+cargo build --release --target wasm32-unknown-unknown
 wasm-bindgen \
   target/wasm32-unknown-unknown/release/game_of_life.wasm \
   --out-dir build \
